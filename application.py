e82d5c7c5600ad10541f9194e6f4de53e9b764f5
Muhammad Rahman <99muhammad.r@gmail.com>
2021-03-02 19:57:24 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+#Add Imports
 from flask import Flask, render_template, url_for, redirect, request
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
@@ -19,6 +20,7 @@ from werkzeug.utils import secure_filename
 import RA
 import radt
 
+#set up R code through rpy2 
 r = robjects.r
 r['source']('R-visuals.R')
 gseFUNC = robjects.globalenv['get_file_name']
@@ -33,19 +35,11 @@ extract.get_important_data()
 # import sql ---> Uncomment if tranfac.db not present
 
 
-# create a flask application object
-# def create_app():
-#     application = Flask(__name__)
-#     Bootstrap(app)
-#     FontAwesome(app)
-#     application.config['SECRET_KEY'] = 'change this unsecure key'
-
-#     return application
+# create a flask application object with Boostrap and FontAwesome capabilities
 def create_app():
     application = Flask(__name__)
     Bootstrap(application)
     FontAwesome(application)
-    application.config['SECRET_KEY'] = 'change this unsecure key'
     application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
     return application
@@ -54,8 +48,7 @@ def create_app():
 application = create_app()
 
 
-# we need to set a secret key attribute for secure forms
-
+#Set up flask form for user input
 class QueryForm(FlaskForm):
     submit = SubmitField()
 
@@ -71,7 +64,7 @@ def index():
 def search():
     form = QueryForm()
     htf_name = None
-
+    #configure searchbar for when user inputs category of either drug or gene name
     if request.method == 'POST':
         category_type = request.form.get('category')
         if category_type == 'tf':
@@ -86,9 +79,11 @@ def search():
 # define actions for tf profiles
 @application.route('/tfprofile/<htf_name>', methods=['GET', 'POST'])
 def tfprofile(htf_name):
+    #extract all symbols
     symbs = extract.get_symbols()
-
+    #identify if the gene symbol entered is present within a list of all the current human transcription factor genes
     if htf_name in symbs:
+        #extract relevant tf information from sql database
         db = SQL("sqlite:///transfacts.db")
 
         htf_name = htf_name.upper()
@@ -96,6 +91,7 @@ def tfprofile(htf_name):
         user_inp = db.execute('''SELECT * FROM Htf_info WHERE Symbol = ?''', htf_name)
 
         u_i = db.execute('''SELECT * FROM Protein_info WHERE Symbol = ?''', htf_name)
+        #Take account for if there is no data present for the input that is entered and provide exception
         try:
 
             dict1_drugs = {}
@@ -109,7 +105,8 @@ def tfprofile(htf_name):
             test_drug_name = "None"
             test_drug_concept_id = "None"
             dict1_drugs[test_drug_concept_id] = test_drug_name
-
+        
+        #extract key transcription factor background information
         for num in range(len(user_inp)):
             ensembl = user_inp[num]['Ensembl']
             chr = user_inp[num]['Chromosome']
@@ -120,13 +117,13 @@ def tfprofile(htf_name):
             symbol = user_inp[num]['Symbol']
             family = user_inp[num]['Family']
             break
-
+        #Extract target gene information that they are known to regulate
         targets = target.get_htf_target_data(htf_name)
 
         return render_template('tfprofile.html', symbol=symbol, ensembl=ensembl, family=family, chr=chr,
                                full_name=full_name, uniprot=uniprot,
                                subcell=subcell, func=func, targets=targets, drug=dict1_drugs)
-
+    #send to error page if no key data found
     else:
         return redirect(url_for('error', htf_name=None))
 
@@ -146,26 +143,32 @@ def download():
 # define action for GEO upload page
 @application.route('/geo',methods=['GET','POST'])
 def geo():
+    #clear R environment
     clearFUNC()
+    #once user uploads file send to POST method
     if request.method == 'POST':
+        #get file name and save in directory 
         f = request.files['file']
         f.save(secure_filename(f.filename))
+        #create heatmap image
         heatmap_create = gseFUNC(secure_filename(f.filename))
 
-
-        a = RA.ge_data(secure_filename(f.filename))
-        a.to_csv("ge_data.csv")
-        b = RA.connec_data(a)
-        b.to_csv('connec_data.csv')
+        #provide gene expression data and human transcription factor CHIP-SEQ matrix data for plsgenomics package in R to estimate transcription factor activity values
+        ge = RA.ge_data(secure_filename(f.filename))
+        ge.to_csv("ge_data.csv")
+        connec = RA.connec_data(a)
+        connec.to_csv('connec_data.csv')
+        #create relative activity table and export as csv to main directory
         raFUNC()
 
-
+        #send user to results page
         return redirect(url_for('geo_results'))
     return render_template('GEO.html')
 
-#
+#define action for geo data visualitions
 @application.route('/geo_results')
 def geo_results():
+    #extract column and row data from relative activity csv file in order to export to html for table format
     colnames = radt.column_names()
     rownames = radt.row_names()
     return render_template('GEO-results.html',  colnames=colnames, rownames=rownames)
@@ -180,15 +183,16 @@ def geo_results():
 # define actions for drug profiles
 @application.route('/drugprofile/<drug_name>', methods=['GET', 'POST'])
 def drug(drug_name):
+    #extract relevant drug information from sql database
     db = SQL("sqlite:///transfacts.db")
     try:
-
+        
         drug_data = db.execute('''SELECT * FROM drug_info WHERE drug_name  = ?''', drug_name)
 
-
+    #Take account for drug data if not found
     except:
         drug_data = [{"drug_name": "None", "ACTION_TYPE": "None", "Symbol": "None", "COMPOUND_NAME": "None"}]
-
+    
     for num in range(len(drug_data)):
         chembl_id = drug_data[num]['CHEMBL_ID']
         drug_name = drug_data[num]['drug_name']
@@ -197,7 +201,7 @@ def drug(drug_name):
         first_approval = drug_data[num]['FIRST_APPROVAL']
 
         break
-
+    
     return render_template('drugprofile.html', drug_data=drug_data, chembl_id=chembl_id, drug_name=drug_name,
                            description=description,
                            molecule_type=molecule_type, first_approval=first_approval)
@@ -206,6 +210,7 @@ def drug(drug_name):
 # define actions for browse page
 @application.route('/browse')
 def tfbrowse():
+    #extract all transcription factor gene symbols and protein names 
     db = SQL("sqlite:///transfacts.db")
 
     tfs = db.execute('''SELECT * FROM browse_info''')
@@ -218,7 +223,7 @@ def tfbrowse():
             symbol = i['Symbol']
             dict1_tfs[symbol] = full_name
     except:
-
+    #account for no data being found in sql database
         dict1_tfs = {}
         full_name = "None"
         symbol = "None"
